b59edfd7f2f948616c729347dd3ce84769d6a360
Mayel de Borniol <git@mayel.space>
2026-08-12 22:39:57 +0100

    fixes

--- test/bonfire_data_content_test.exs
+++ test/bonfire_data_content_test.exs
@@ -1,5 +1,8 @@
 defmodule Bonfire.Pages.Test do
   use ExUnit.Case
 
+  # bucket this into the backend CI leg: bare `ExUnit.Case` skips the tag the extension case templates apply, so without it this also runs in the federation job catch-all
+  @moduletag :backend
+
   # TODO
 end
